bef0b8e8571e558517d61aae10eff604d9f053ad
WenM15 <wenmingleong01@gmail.com>
2026-04-18 01:30:29 +0800

    Improve process ref. Do process dict, ques. Do ques1 in Start. Update UI.

--- logistics_calculator/MainWindow.xaml.cs
+++ logistics_calculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ExcelDataReader;
+﻿using ClosedXML.Excel;
+using ExcelDataReader;
 using Microsoft.Win32;
 using System.IO;
 using System.Text;
@@ -12,164 +13,184 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// (TODO) Might not need `bool abort` variable
+
 namespace logistics_calculator
-{
-    using Dict4 = System.Collections.Generic.Dictionary<Weight, double>;
-    using Dict3 = System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<Weight, double>>;
-    using Dict2 = System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<Weight, double>>>;
-    using Dict1 = System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<Weight, double>>>>;
+{   
+    using WeightMap = List<double>;
+    using HAWB_Map = System.Collections.Generic.Dictionary<int, List<double>>;
+    using QuesRegionMap = System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<int, List<double>>>;
+
+    using WeightRangeMap = System.Collections.Generic.Dictionary<Weight, double>;
+    using ServiceLevelMap = System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<Weight, double>>;
+    using RegionMap = System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<Weight, double>>>;
+    
+    
+
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Dictionary<string, int> ref_header_map = new Dictionary<string, int>();
+        private RegionMap ref_region_map = new RegionMap();
+        private string target_service_level = "PRIORITY OVERNIGHT OR INTERNATIONAL PRIORITY";
+
+        private Dictionary<string, string> alias_map = new Dictionary<string, string>();
+        private QuesRegionMap ques_region_map = new QuesRegionMap();
+        private Dictionary<string, int> ques_header_map = new Dictionary<string, int>();
+
+        private string? outputFolderPath = null;
+
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
+            target_service_level = target_service_level.Replace(" ", "");
+        }
+
+        private void ProcessDictFile(object sender, RoutedEventArgs e)
+        {
+            string? filePath = GetFileFromDialog();
+            if (filePath == null)
+            {
+                // sad path
+                return;
+            }
+
+            foreach(string line in File.ReadLines(filePath))
+            {
+                string[] pair = line.Split('=');
+                alias_map.Add(pair[0], pair[1]);
+            }
         }
 
         private void ProcessRefFile(object sender, RoutedEventArgs e)
         {
-            string file_path = null;
-            OpenFileDialog dialog = new OpenFileDialog();
-
-            if (dialog.ShowDialog() == false)
+            // Get file
+            string? file_path = GetFileFromDialog();
+            if (file_path == null)
             {
+                // sad path
                 return;
             }
 
-            file_path = dialog.FileName;
-
+            // Get reader
             FileStream stream = File.Open(file_path, FileMode.Open, FileAccess.Read);
             var reader = ExcelReaderFactory.CreateReader(stream);
 
-            #region get header
-            // Get header first            
-            Dictionary<string, int> header_index_map = new Dictionary<string, int>();
-            header_index_map["DestCountry/RegionCode"] = 0;
-            header_index_map["ServiceLevelShortDescription"] = 0;
-            header_index_map["PackageTypeCode"] = 0;
-            header_index_map["MinWeight"] = 0;
-            header_index_map["MaxWeight"] = 0;
-            header_index_map["Rate"] = 0;
+            // Declare headers            
+            ref_header_map["DestCountry/RegionCode"] = 0;
+            ref_header_map["ServiceLevelShortDescription"] = 0;
+            ref_header_map["PackageTypeCode"] = 0;
+            ref_header_map["MinWeight"] = 0;
+            ref_header_map["MaxWeight"] = 0;
+            ref_header_map["Rate"] = 0;
 
+            #region get column index based on header
             int found = 0;
-            bool all_correct = false;
             while (reader.Read())
             {
-                int column = 0;
-                while (true)
+                for (int column = 0; column < reader.FieldCount; ++column)
                 {
-                    var value = reader.GetValue(column);
+                    string? value = reader.GetValue(column)?.ToString();
+
                     if (value == null)
                     {
-                        break;
-                    }
-                    string value_string = value.ToString();
-                    if (string.IsNullOrWhiteSpace(value_string))
-                    {
-                        break;
+                        continue;
                     }
 
-                    // Use dictionary key to update value
-                    if (header_index_map.ContainsKey(value_string))
+                    if (ref_header_map.ContainsKey(value))
                     {
-                        header_index_map[value_string] = column;
                         ++found;
+                        ref_header_map[value] = column;                        
                     }
-
-                    if (found == header_index_map.Count)
-                    {
-                        all_correct = true;
-                        break;
-                    }
-
-                    ++column;
                 }
                 if (found > 0)
                 {
                     break;
                 }
             }
-            if (!all_correct)
+
+            if (found != ref_header_map.Count)
             {
                 return;
             }
             #endregion
 
-            Dict1 dict1 = new Dict1();
-
             bool abort = false;
             while (reader.Read())
             {
-                string region = reader.GetValue(header_index_map["DestCountry/RegionCode"])?.ToString() ?? "";
-                string service_level = reader.GetValue(header_index_map["ServiceLevelShortDescription"])?.ToString() ?? "";
-                string package_type = reader.GetValue(header_index_map["PackageTypeCode"])?.ToString() ?? "";
-                string min_weight_str = reader.GetValue(header_index_map["MinWeight"])?.ToString() ?? "";
-                string max_weight_str = reader.GetValue(header_index_map["MaxWeight"])?.ToString() ?? "";
-                string rate_str = reader.GetValue(header_index_map["Rate"])?.ToString() ?? "";
-
-                region = region.Replace(" ", "");
+                #region retrieve values
+                string? service_level = reader.GetValue(ref_header_map["ServiceLevelShortDescription"])?.ToString();
+                if (service_level == null)
+                {   
+                    // sad path
+                    abort = true;
+                    break;
+                }
                 service_level = service_level.Replace(" ", "");
-                package_type = package_type.Replace(" ", "");
-                min_weight_str = min_weight_str.Replace(" ", "");
-                max_weight_str = max_weight_str.Replace(" ", "");
-                rate_str = rate_str.Replace(" ", "");
+                if (service_level != target_service_level)
+                {
+                    // unintended path
+                    continue;
+                }
+
+                string? region = reader.GetValue(ref_header_map["DestCountry/RegionCode"])?.ToString();                                
+                string? min_weight_str = reader.GetValue(ref_header_map["MinWeight"])?.ToString();
+                string? max_weight_str = reader.GetValue(ref_header_map["MaxWeight"])?.ToString();
+                string? rate_str = reader.GetValue(ref_header_map["Rate"])?.ToString();
+
+                if (region == null || min_weight_str == null || max_weight_str == null || rate_str == null)
+                {
+                    // sad path
+                    abort = true; 
+                    break;
+                }
 
                 Weight weight = new Weight();
-                if (double.TryParse(min_weight_str, out double min_weight))
+                if (double.TryParse(min_weight_str, out double min_weight) == false)
                 {
-                    if (double.TryParse(max_weight_str, out double max_weight))
-                    {
-                        weight.MinWeight = min_weight;
-                        weight.MaxWeight = max_weight;
-                    }
-                    else
-                    {
-                        abort = true;
-                        break;
-                    }
-                }
-                else
-                {
+                    // sad path
                     abort = true;
                     break;
                 }
-
-                if (!double.TryParse(rate_str, out double rate))
+                if (double.TryParse(max_weight_str, out double max_weight) == false)
                 {
+                    // sad path
                     abort = true;
                     break;
                 }
+                weight.MinWeight = min_weight;
+                weight.MaxWeight = max_weight;
 
-                if (!dict1.ContainsKey(region))
+                if (double.TryParse(rate_str, out double rate) == false)
                 {
-                    dict1[region] = new Dict2();
+                    // sad path
+                    abort = true;
+                    break;
                 }
+                #endregion
 
-                if (!dict1[region].ContainsKey(service_level))
+                #region store values
+                if (ref_region_map.ContainsKey(region) == false)
                 {
-                    dict1[region][service_level] = new Dict3();
+                    ref_region_map.Add(region, new ServiceLevelMap());
                 }
-
-                if (!dict1[region][service_level].ContainsKey(package_type))
+                if (ref_region_map[region].ContainsKey(service_level) == false)
                 {
-                    dict1[region][service_level][package_type] = new Dict4();
+                    ref_region_map[region].Add(service_level, new WeightRangeMap());
                 }
-
-                if (!dict1[region][service_level][package_type].ContainsKey(weight))
+                if (ref_region_map[region][service_level].ContainsKey(weight) == false)
                 {
-                    dict1[region][service_level][package_type] = new Dict4();
+                    ref_region_map[region][service_level].Add(weight, rate);
                 }
-
-                if (dict1[region][service_level][package_type][weight] != rate)
-                {
-                    dict1[region][service_level][package_type][weight] = rate;
-                }
+                #endregion
             }
 
-            if (!abort)
+            if (abort == true)
             {
+                // sad path
                 return;
             }
 
@@ -179,12 +200,213 @@ namespace logistics_calculator
 
         private void ProcessQuesFile(object sender, RoutedEventArgs e)
         {
+            // Get file
+            string? file_path = GetFileFromDialog();
+            if (file_path == null)
+            {
+                // sad path
+                return;
+            }
 
+            // Get reader
+            FileStream stream = File.Open(file_path, FileMode.Open, FileAccess.Read);
+            var reader = ExcelReaderFactory.CreateReader(stream);
+
+            // Declare headers
+            ques_header_map["Ship-ToID"] = 0;
+            ques_header_map["HAWB/BOL"] = 0;
+            ques_header_map["GW"] = 0;
+
+            #region get column index based on header
+            int found = 0;
+            while (reader.Read())
+            {
+                for (int column = 0; column < reader.FieldCount; ++column)
+                {
+                    string? value = reader.GetValue(column)?.ToString();
+
+                    if (value == null)
+                    {
+                        continue;
+                    }
+
+                    if (ques_header_map.ContainsKey(value))
+                    {
+                        ++found;
+                        ques_header_map[value] = column;
+                    }
+                }
+                if (found > 0)
+                {
+                    break;
+                }
+            }
+
+            if (found != ques_header_map.Count)
+            {
+                return;
+            }
+            #endregion
+
+            bool abort = false;
+            while (reader.Read())
+            {
+                #region retrieve values
+                string? ship_to_id = reader.GetValue(ques_header_map["Ship-ToID"])?.ToString();
+                string? hawb_str = reader.GetValue(ques_header_map["HAWB/BOL"])?.ToString();
+                string? weight_str = reader.GetValue(ques_header_map["GW"])?.ToString();
+
+                if (ship_to_id == null || hawb_str == null || weight_str == null)
+                {
+                    // sad path
+                    abort = true;
+                    break;
+                }
+
+                if (int.TryParse(hawb_str, out int hawb) == false)
+                {
+                    // sad path
+                    abort = true;
+                    break;
+                }
+                if (double.TryParse(weight_str, out double weight) == false)
+                {
+                    // sad path
+                    abort = true;
+                    break;
+                }
+                #endregion
+
+                #region store values
+                if (ques_region_map.ContainsKey(ship_to_id) == false)
+                {
+                    ques_region_map.Add(ship_to_id, new HAWB_Map());
+                }
+                if (ques_region_map[ship_to_id].ContainsKey(hawb) == false)
+                {
+                    ques_region_map[ship_to_id].Add(hawb, new WeightMap());
+                }
+                if (ques_region_map[ship_to_id][hawb].Contains(weight) == false)
+                {
+                    ques_region_map[ship_to_id][hawb].Add(weight);
+                }
+                #endregion
+            }
+            if (abort == true)
+            {
+                return;
+            }
+
+            reader.Close();
+            stream.Close();
+        }
+
+        private string? GetFileFromDialog()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == false)
+            {
+                return null;
+            }
+            return dialog.FileName;
+        }
+
+        private string? GetFolderFromDialog()
+        {
+            OpenFolderDialog dialog = new OpenFolderDialog();
+            if (dialog.ShowDialog() == false)
+            {
+                return null;
+            }
+            return dialog.FolderName;
+        }
+
+        private void SelectOutputFolder(object sender, RoutedEventArgs e)
+        {
+            outputFolderPath = GetFolderFromDialog();
         }
 
         private void Start(object sender, RoutedEventArgs e)
-        {
+        {         
+            if (outputFolderPath == null || outputFileTextBox.Text == null)
+            {
+                // sad path
+                return;
+            }
 
+            // Initialize excel file
+            var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("Sheet1");
+
+            // Add header to sheet
+            int row = 1;
+            int col = 1;
+            foreach (string? header in ques_header_map.Keys)
+            {
+                worksheet.Cell(row, col).Value = header;
+                ++col;
+            }
+            worksheet.Cell(row, col).Value = "Cost";
+            ++row;
+            col = 1;
+
+            // get individual cost
+            foreach (var dest_hawb_pair in ques_region_map)
+            {                
+                string ship_to_id = dest_hawb_pair.Key;
+                HAWB_Map hawb_map = dest_hawb_pair.Value;
+
+                string region;
+                if (alias_map.ContainsKey(ship_to_id))
+                {
+                    region = alias_map[ship_to_id];
+                }
+                else
+                {
+                    // sad path
+                    break;
+                }
+
+                foreach (var hawb_weight_pair in hawb_map)
+                {
+                    int hawb = hawb_weight_pair.Key;
+                    WeightMap weight_map = hawb_weight_pair.Value;
+
+                    foreach (double weight in weight_map)
+                    {
+                        WeightRangeMap weight_range_map = ref_region_map[region][target_service_level];
+                        foreach (KeyValuePair<Weight, double> weight_rate_pair in weight_range_map)
+                        {
+                            double min_weight = weight_rate_pair.Key.MinWeight;
+                            double max_weight = weight_rate_pair.Key.MaxWeight;
+                            double rate = weight_rate_pair.Value;
+
+                            if ((weight >= min_weight) && (weight <= max_weight))
+                            {                                                                
+                                double cost = rate * weight;
+
+                                worksheet.Cell(row, col).Value = ship_to_id;
+                                ++col;
+                                worksheet.Cell(row, col).Value = hawb;
+                                ++col;
+                                worksheet.Cell(row, col).Value = weight;
+                                ++col;
+                                worksheet.Cell(row, col).Value = cost;
+
+                                ++row;
+                                col = 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Write excel file to disk
+            string fullPath = System.IO.Path.Combine(outputFolderPath, outputFileTextBox.Text);
+            workbook.SaveAs($"{fullPath}.xlsx");
+
+            workbook.Dispose();
         }
     }
 }
